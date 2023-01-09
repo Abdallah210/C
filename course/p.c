d83cf8e766c295567c9cbccf4f0b4631172c335d
Elfilali210 <elfilali210@hotmail.com>
2023-01-09 18:06:55 +0100

    course

---
+++ course/p.c
@@ -0,0 +1,39 @@
+/* c e c i e s t un commentai re */ 
+
+
+/* 
+#include : directive au préprocesseur : 
+permet l'usage de la fonction printf() 
+*/
+#include <stdio.h>
+
+int main ( void ) {
+    printf ("Salut le monde \n") ; 
+    return 0 ;                 
+}                               
+
+
+/* 
+1- include :
+Syntax :
+#include <fichier_en_tête.h>   or    #include "chemin_du_fichier_en_tête"
+
+Examples :
+    #include <ctype.h>    analyse et transformation de caractères.
+    #include <math.h>     fonctions mathématiques.
+    #include <setjmp.h>   traitement des erreurs.
+    #include <signal.h>   gestion des événements.
+    #include <string.h>   fonctions de manipulations de chaŽnes de caractères.
+    #include <time.h>     fonctions relatives au temps qui permettent de gérer des temporisations.
+
+
+2- main() :
+syntax :
+type main (arguments) {
+    ...
+    ...
+    ...
+}
+
+
+*/
