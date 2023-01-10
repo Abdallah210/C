bbb51d48e8df701be3c2d37fe5df84b3b82e0aa9
Elfilali210 <elfilali210@hotmail.com>
2023-01-10 12:18:53 +0100

    hello world

---
+++ course/helloWorld.c
@@ -0,0 +1,39 @@
+/* this is a comment */ 
+
+
+/* 
+#include : directive to preprocessor: 
+allows the use of the printf() function 
+*/
+#include <stdio.h>
+
+int main ( void ) {
+    printf ("Hello World !\n") ; 
+    return 0 ;                 
+}                               
+
+
+/* 
+1- include :
+Syntax :
+#include <header_file.h>   or    #include "path_of_header_file"
+
+Examples :
+    #include <ctype.h>    character analysis & transformation.
+    #include <math.h>     mathematical functions.
+    #include <setjmp.h>   traitement des erreurs.
+    #include <signal.h>   event management.
+    #include <string.h>   string manipulation functions.
+    #include <time.h>     time-related functions that manage time delays.
+
+
+2- main() :
+syntax :
+type main (arguments) {
+    ...
+    ...
+    ...
+}
+
+
+*/
