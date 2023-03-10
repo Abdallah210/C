9aabb9e1279cc8f53f3c28eadaafa9ecf6ceebcd
Elfilali210 <elfilali210@hotmail.com>
2023-03-10 19:55:01 +0100

    test

---
+++ course/test.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    int nombreDeVies;
+    unsigned int nombrePositif;
+
+    int age = 0;
+
+    /*
+    //printf("inserez votre age :\n");
+    //scanf("%d", &age);  // change the value of the variable 
+    //printf("\nMon age est %d", age);
+    */
+
+   /*
+    int resultat = 0, nombre1 = 0, nombre2 = 0;
+    printf("inserer un premier nombre : ");
+    scanf("%d", &nombre1);
+    printf("inserer un deuxieme nombre : ");
+    scanf("%d", &nombre2);
+
+    resultat = nombre1 + nombre2;
+    printf("La somme du %d et %d est : %d", nombre1, nombre2, resultat);
+    */
+
+   /*
+    //incrementation :
+    age = age + 1;
+    age++;
+    printf("age doit etre 2 --> %d", age);
+    */
+   
+   /*
+   int choix;
+   printf("=== Menu ===\n");
+   printf("1. Royal Cheese\n");
+   printf("2. Mc Deluxe\n");
+   printf("3. Mc Bacon\n");
+   printf("4. Big Mac\n");
+   printf("Votre choix ?\n");
+
+   scanf("%d", &choix);
+
+   switch (choix)
+   {
+   case 1:
+       printf("tu as choisi Royal Cheese");
+    break;
+   case 2:
+       printf("tu as choisi Mc Deluxe");
+    break;
+   case 3:
+       printf("tu as choisi Mc Bacon");
+    break;
+   case 4:
+       printf("tu as choisi Big Mac");
+    break;
+   default:
+       printf("Ce choix n'est pas disponible.");
+    break;
+   }
+   */
+
+
+    return 0;
+}
