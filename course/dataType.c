060200132d884fa9c4c20827e39dc4857fcac780
Elfilali210 <elfilali210@hotmail.com>
2023-03-11 13:14:36 +0100

    data type

---
+++ course/dataType.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+
+int main()
+{
+    
+    char a = 'A';            // single character 
+    char b[] = "Abdallah";    // array of characters
+    
+    float c = 3.141592;           // 4 bytes (32 bits of precision) 6-7 digits
+    double d = 3.141592653589793; // 8 bytes (64 bits of precision) 15-16 digits 
+    
+    // should import <stdbool.h> to use bool type : 
+    bool e = true;
+    
+    
+    return 0;
+}
